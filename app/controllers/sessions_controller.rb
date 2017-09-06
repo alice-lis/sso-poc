cb25f53b4ffb5b014afdea38103c470bea41dd97
Eugene Grechuk <eg@anadeainc.com>
2017-09-06 15:25:33 +0300

    Change session create flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     session[:user_id] = auth[:uid]
     session[:provider] = params[:name]
-    redirect_to(params[:RelayState] || '/')
+    redirect_to(params[:redirectUrl] || '/')
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
 
-  post '/auth/:name/callback', to: 'sessions#create'
-  match '/session', to: 'sessions#destroy', via: :get
+  get '/session/create', to: 'sessions#create'
+  get '/session', to: 'sessions#destroy'
 
   get 'okta', to: 'okta#index'
   get 'onelogin', to: 'onelogin#index'
